33b0affaba6add9eec1d1fc5126310971486847c
Vincent-Mpenyana <vincent.mpenyana8@gmail.com>
2020-05-01 15:32:13 +0200

    Updates

---
+++ precache-manifest.ed9a2f1ed85d427e7f618b6f8dafc6a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caed08fc6ed21762c631e3957325d528",
+    "url": "/facebrain/index.html"
+  },
+  {
+    "revision": "1ce8029b1025ead51a4c",
+    "url": "/facebrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "164a5aad561056f62fd9",
+    "url": "/facebrain/static/css/main.3e3d5b0f.chunk.css"
+  },
+  {
+    "revision": "1ce8029b1025ead51a4c",
+    "url": "/facebrain/static/js/2.eddf9f79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facebrain/static/js/2.eddf9f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "164a5aad561056f62fd9",
+    "url": "/facebrain/static/js/main.f052a074.chunk.js"
+  },
+  {
+    "revision": "627e1779b32a0bc14d06",
+    "url": "/facebrain/static/js/runtime-main.1c7e8e86.js"
+  }
+]);
